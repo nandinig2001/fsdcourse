40dff5e5f05e51ad37fc98c9861bca2a03f93ac8
hanil <haniljain@gmail.com>
2021-09-22 12:30:08 +0530

    added loginpage and its authentication

--- fsd/course/urls.py
+++ fsd/course/urls.py
@@ -4,5 +4,8 @@ from django.conf import settings
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
-  
+    path('register',views.registerPage,name='register'),
+    path('login',views.loginPage,name='login'),
+    path('logout',views.logoutPage,name='logout'),
+
 ]
--- fsd/course/views.py
+++ fsd/course/views.py
@@ -4,7 +4,41 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.mail import send_mail, EmailMessage
 from django.contrib import messages
-
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+from django.contrib.auth import authenticate,login, logout
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def registerPage(request):
+    form = CreateUserForm()
+    if request.method=='POST':
+        print("Successful")
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request,'Account was created for'+user)
+
+            return redirect('login')
+    context ={'form':form}
+    return render(request,'register.html',context)
+
+def loginPage(request):
+    if request.method == 'POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user = authenticate(request, password=password, username=username)
+        if user is not None:
+            login(request,user)
+            print("Loggeg In")
+            return redirect('index')
+        else:
+          messages.info(request, 'Username OR password is incorrect')
+    context = {}
+    return render(request,'login.html')
+
+def logoutPage(request):
+    logout(request)
+    return redirect('login')
